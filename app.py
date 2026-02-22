6d4a2b9180b65a336029bfac5b19377c90c91e8f
xandrali93-cpu <xandrali93@gmail.com>
2026-02-23 00:34:12 +0500

    Update app.py

--- app.py
+++ app.py
@@ -119,6 +119,7 @@ def generate_pdf(data):
    
         
     if 'Date' in data.columns and 'Amount' in data.columns:
+        trend_data = data.groupby('Date')['Amount'].sum().reset_index()
         plt.figure(figsize=(10, 5))
         plt.plot(trend_data['Date'], trend_data['Amount'], marker='o', color='tab:blue')
         plt.savefig("temp_line.png", format='png', bbox_inches='tight')
@@ -159,6 +160,7 @@ if 'Date' in df.columns and 'Amount' in df.columns and 'Category' in df.columns:
     
 else:
     st.warning("⚠️ Для создания отчета в файле должны быть колонки с датой, суммой и названием товаров.")
+
 
 
 
